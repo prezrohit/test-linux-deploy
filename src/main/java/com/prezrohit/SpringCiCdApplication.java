4e6a5c139f0f3be9932d8e8eb2a1e61d3922c4ac
prezrohit <rohitsr80@gmail.com>
2025-01-14 13:06:37 +0530

    Initial Commit

---
+++ src/main/java/com/prezrohit/SpringCiCdApplication.java
@@ -0,0 +1,21 @@
+package com.prezrohit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCiCdApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
